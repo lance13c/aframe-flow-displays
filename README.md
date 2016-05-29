59d541acd49cb79548ae93c6ee72f82b3157b290
lance13c <pianoman13c@gmail.com>
2016-05-29 13:11:08 -0400

    Removed content from gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# aframe-flow-displays
-Using aframe, create 3D modular displays users can intuitively navigate just by looking around in VR.
--- src/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Lance on 5/28/2016.
- */
-require('aframe-text-component');
-const $ = require('jquery');
-const mainSass = require('./style/sass/main.scss');
-const click_listener = require('./js/components/click_listener.js');
